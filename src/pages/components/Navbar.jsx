997a42872af1af6357723f9913f1db842d661a3a
mosefatullah <mosefatullah@gmail.com>
2023-08-13 14:23:56 +0600

    ...

--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -18,7 +18,7 @@ function Navbar() {
         <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
        </svg>
       </button>
-      <h2>Wetube</h2>
+      <h2>WeTube</h2>
      </div>
      <div className="input-group">
       <input
