f063290b54c00efac48d5f240fb9bf7eb8297a84
mosefatullah <mosefatullah@gmail.com>
2023-08-15 00:03:06 +0600

    .

--- src/pages/Watch.jsx
+++ src/pages/Watch.jsx
@@ -49,10 +49,87 @@ function Watch() {
        <Videobox src={thumbnailUrl} title={title} channelSrc={channelSrc} />
       </div>
       <div className="col-lg-4">
-       <div className="card card-body text-black">
-        <h3>Description</h3>
+       <div className="card card-body bg-dark">
+        <h2>{title}</h2>
+        <h3>8,007,680 views  -  19 Oct 2022</h3>
         <hr />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, pariatur amet sit sed impedit nulla eos dicta ducimus eligendi cupiditate quod voluptatem sequi beatae incidunt esse maiores porro repellat et.</p>
+        <p>
+         Learn the basics of computer science from Harvard University. This is
+         CS50, an introduction to the intellectual enterprises of computer
+         science and the art of programming.
+         <br />
+         💻 Slides, source code, and more at https://cs50.harvard.edu/x.
+         <br />
+         ⭐️ Course Contents ⭐️ ⌨️ (00:00:00) Lecture 0 - Scratch <br />
+         ⌨️ (01:45:08) Lecture 1 - C <br />
+         ⌨️ (04:13:23) Lecture 2 - Arrays <br />
+         ⌨️ (06:20:43) Lecture 3 - Algorithms <br />
+         ⌨️ (08:37:55) Lecture 4 - Memory <br />
+         ⌨️ (11:03:17) Lecture 5 - Data Structures <br />
+         ⌨️ (13:15:36) Lecture 6 - Python <br />
+         ⌨️ (15:39:25) Lecture 7 - SQL <br />
+         ⌨️ (18:00:55) Lecture 8 - HTML, CSS, JavaScript <br />
+         ⌨️ (20:23:38) Lecture 9 - Flask <br />
+         ⌨️ (22:39:01) Lecture 10 - Emoji <br />
+         ⌨️ (24:02:50) Cybersecurity <br />
+         <br />
+         Recorded in 2021.
+         <br />
+         ---
+         <br />
+         HOW TO JOIN CS50 COMMUNITIES
+         <br />
+         Discord: https://discord.gg/cs50 <br />
+         Ed: https://cs50.harvard.edu/x/ed <br />
+         Facebook Group: https://www.facebook.com/groups/cs50/ <br />
+         Faceboook Page: https://www.facebook.com/cs50/ <br />
+         GitHub: https://github.com/cs50 <br />
+         Gitter: https://gitter.im/cs50/x <br />
+         Instagram: https://instagram.com/cs50 <br />
+         LinkedIn Group: https://www.linkedin.com/groups/7437240/ <br />
+         LinkedIn Page: https://www.linkedin.com/school/cs50/ <br />
+         Medium: https://cs50.medium.com/ <br />
+         Quora: https://www.quora.com/topic/CS50 <br />
+         Reddit: https://www.reddit.com/r/cs50/ <br />
+         Slack: https://cs50.edx.org/slack <br />
+         Snapchat: https://www.snapchat.com/add/cs50 <br />
+         SoundCloud: https://soundcloud.com/cs50 <br />
+         Stack Exchange: https://cs50.stackexchange.com/ <br />
+         TikTok: https://www.tiktok.com/@cs50 <br />
+         Twitter: https://twitter.com/cs50 <br />
+         YouTube:
+         <br />
+         / cs50
+         <br />
+         HOW TO FOLLOW DAVID J. MALAN
+         <br />
+         Facebook: https://www.facebook.com/dmalan <br />
+         GitHub: https://github.com/dmalan <br />
+         Instagram: https://www.instagram.com/davidjmalan/ <br />
+         LinkedIn: https://www.linkedin.com/in/malan/ <br />
+         TikTok: https://www.tiktok.com/@davidjmalan <br />
+         Twitter: https://twitter.com/davidjmalan <br />
+         <br />
+         LICENSE
+         <br />
+         CC BY-NC-SA 4.0 Creative Commons Attribution-NonCommercial-ShareAlike
+         4.0 International Public License <br />
+         https://creativecommons.org/licenses/...
+         <br />
+         🎉 Thanks to our Champion and Sponsor supporters: <br />
+         👾 Raymond Odero <br />
+         👾 Agustín Kussrow <br />
+         👾 aldo ferretti <br />
+         👾 Otis Morgan <br />
+         👾 DeezMaster <br />
+         <br />
+         --
+         <br />
+         Learn to code for free and get a developer job:
+         https://www.freecodecamp.org
+         <br />
+         Read hundreds of articles on programming: https://freecodecamp.org/news
+        </p>
        </div>
       </div>
      </div>
